ba0d1445c4a6e711a30af7838f041d1bdf0842e4
IngridAMorales <ingridmorales1016@gmail.com>
2021-10-25 13:15:38 -0700

    Updated src/rectangle.cpp

---
+++ src/rectangle.cpp
@@ -0,0 +1,21 @@
+#include "../header/rectangle.hpp"
+
+Rectangle::Rectangle(int w, int h){
+    int width = set_width(w);
+    int height = set_height(h);
+}
+void Rectangle::set_width(int w) {
+    this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+    this->height = h;
+}
+
+int Rectangle::area() {
+    return this->width * this->height;
+}
+int Rectangle::perimeter(){
+    return this->width*2 + this->height*2
+}
+
